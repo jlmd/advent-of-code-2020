ae2c8f189b1d7a451ba37851e57935c53c7b6ff4
Jose Luis Martin <joseluis.martind@gmail.com>
2020-12-06 20:18:54 +0100

    Add day 06 part 1

---
+++ day06/day06part1.py
@@ -0,0 +1,20 @@
+def count_unique_chars(chars):
+    chars_occ = {}
+    for char in chars:
+        chars_occ[char] = True
+    return len(chars_occ)
+
+
+if __name__ == '__main__':
+    unique_chars_count = 0
+    with open('input.txt', 'r') as f:
+        line = f.readline()
+        chars = ""
+        while line:
+            chars += line.replace("\n", "")
+            if line == "\n":
+                unique_chars_count += count_unique_chars(chars)
+                chars = ""
+            line = f.readline()
+    unique_chars_count += count_unique_chars(chars)
+    print unique_chars_count
